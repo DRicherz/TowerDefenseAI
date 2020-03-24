1f1a12ee42cac49ee47349178a81e825bae35091
DRicherz <dennisricherzhagen@googlemail.com>
2020-03-24 18:30:30 +0100

    Add files via upload

---
+++ DennisSoldier.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using GameFramework;
+using System.Xml;
+
+namespace AI_Strategy
+{
+    //Basic node class with some added functionality
+    //gCosts: Straight movement = 1, diagonal movement Sqrt(2)
+    //Good practice: multiply by 10 -> straight = 10, diagonal = 14
+    public class Node
+    {
+        
+        public int xPos;
+        public int yPos;
+        public int hurtValue;      
+        public int gCost;       // gCost = Distance to next node
+        public int hCost;       // hCost = Distance to end node   
+
+        public Node parent;
+        public Node(int _xPos, int _yPos, int _hurtValue)
+        {
+            xPos = _xPos;
+            yPos = _yPos;
+            hurtValue = _hurtValue;
+            
+        }
+
+        //Compare two nodes, check if xPosition and yPosition are equal, if they are, it is the same node
+        public bool CompareNode(Node a, Node b)
+        {
+            
+            if(a.xPos == b.xPos && a.yPos == b.yPos) return true;
+
+            return false;
+            
+        }
+
+        //fCost is gCost + hCost
+        public int fCost
+        {
+            get { return gCost + hCost; }
+            
+        }
+    }
+
+    public class Grid
+    {
+        public Node[,] grid;
+
+        //Create a new grid and assign the position and hurt values to the nodes
+        public Node[,] CreateGrid(int[,] hurtMap)
+        {         
+            grid = new Node[PlayerLane.WIDTH, PlayerLane.HEIGHT];         
+            for (int x = 0; x < PlayerLane.WIDTH; x++)
+            {
+                for (int y = 0; y < PlayerLane.HEIGHT; y++)
+                {
+                    grid[x, y] = new Node(x, y, hurtMap[x, y]);                                     
+                }
+            }
+            return grid;
+        }
+
+        /* Get the neighbours of a node in a 3x3 grid
+         * Example: N = Node, X = Neighbours
+         * X-X-X
+         * X-N-X
+         * X-X-X         
+         */
+        public List<Node> GetNeighbours(Node node)
+        {
+            List<Node> neighbours = new List<Node>();
+          
+            for(int x = -1; x <= 1; x++)
+            {
+                for( int y = -1; y <= 1; y++)
+                {
+                    //Discard parameter node
+                    if (x == 0 && y == 0)
+                    {
+                        continue;
+                    }
+                    int tempX = node.xPos + x;
+                    int tempY = node.yPos + y;
+                    
+                    //Prevent null exception
+                    if (tempX >= 0 && tempX < PlayerLane.WIDTH && tempY >= 0 && tempY < PlayerLane.HEIGHT)
+                    {                       
+                        neighbours.Add(grid[tempX, tempY]);                       
+                    }       
+                }
+            }
+            return neighbours;
+        }
+
+        //Debug Method for Grid
+        public void Printgrid(Grid grid) {
+
+            for (int i = 0; i < PlayerLane.HEIGHT; i++)
+            {
+                Console.Write("\n");
+                for (int k = 0; k < PlayerLane.WIDTH; k++)
+                {
+                     Console.Write("|" + "( " + grid.grid[k,i].xPos + "-" + grid.grid[k,i].yPos + ")" + "-" + grid.grid[k,i].gCost + "-" + grid.grid[k, i].hCost);        
+                }
+            }
+                Console.ForegroundColor = ConsoleColor.White;          
+        }
+    }
+    
+    
+    //Based on A* but weighted towards my hurt values 
+    public class PathFinding
+    {
+       
+        public Grid newGrid = new Grid();
+        public List<Node> foundPath = new List<Node>();      
+        
+        public void FindPath(int startX, int startY, int targetX, int targetY, int[,] hurtMap)
+        {
+            newGrid.grid = newGrid.CreateGrid(hurtMap);
+            
+            Node startNode = new Node(startX, startY, hurtMap[startX, startY]);
+            Node targetNode = new Node(targetX, targetY, hurtMap[targetX, targetY]);
+            
+            List<Node> openSet = new List<Node>();
+            HashSet<Node> closedSet = new HashSet<Node>();
+
+            openSet.Add(startNode);
+            
+            while (openSet.Count > 0)
+            {   
+                
+                //Set the currentNode to the first node in the openSet
+                Node currentNode = openSet[0];
+                
+                for (int i = 1; i < openSet.Count; i++)
+                {                  
+                    
+                    if(openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost) 
+                    {
+                        if(openSet[i].hCost < currentNode.hCost )
+                        {
+                            currentNode = openSet[i];
+                            
+                        }
+                    }
+                }
+                
+                openSet.Remove(currentNode);
+                closedSet.Add(currentNode);
+                
+                //If the curretNode is the targetNode a path has been found
+                //Retrace it, see method
+                if(currentNode.CompareNode(currentNode, targetNode))
+                {                  
+                    RetracePath(startNode, targetNode);
+                    return;
+                }
+                
+                foreach (Node neighbour in newGrid.GetNeighbours(currentNode))
+                {
+                    if (closedSet.Contains(neighbour))
+                    {
+                        continue;
+                    }
+
+                    //Heavily weighted towards the hurtValue
+                    //Calculate the movement cost to the neighbour
+                    int movementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour) + 5*currentNode.hurtValue;    
+                    
+
+                    if(movementCostToNeighbour < neighbour.gCost + 5 * currentNode.hurtValue || !openSet.Contains(neighbour))
+                    {
+                        neighbour.gCost = movementCostToNeighbour;
+                        neighbour.hCost = GetDistance(neighbour, targetNode);                      
+                        neighbour.parent = currentNode;
+
+                        if (!openSet.Contains(neighbour))
+                        {
+                            openSet.Add(neighbour);
+                        }
+                        
+                    }
+                }     
+            }          
+        }
+
+
+        //Trace back the chosen path by starting from the endnode
+        //Adding the current node to a list and setting the current node ([0] = endnode) to the nextNode.parent 
+        //Repeat until the start node is the current node
+        //Reverse the list to get the path
+        void RetracePath(Node startNode, Node endNode)
+        {
+            List<Node> path = new List<Node>();
+
+            Node startingNode = newGrid.grid[startNode.xPos, startNode.yPos];
+            Node node = newGrid.grid[endNode.xPos, endNode.yPos];
+
+            while (node != startingNode && node != null)
+            {               
+                path.Add(node);
+                node = node.parent;
+            }
+
+            path.Reverse();
+            foundPath = path;
+        }
+
+        //Get the Distance between two nodes
+        int GetDistance(Node nodeA, Node nodeB)
+        {
+            int distanceX = Math.Abs(nodeA.xPos - nodeB.xPos);
+            int distanceY = Math.Abs(nodeA.yPos - nodeB.yPos);
+
+            if (distanceX > distanceY) { return (14 * distanceY) + (10 * (distanceX - distanceY)); }
+            else
+            {
+                return (14 * distanceX) + (10 * (distanceY - distanceX));
+            }        
+        }
+       
+    }
+    
+    /*
+     * This class derives from Soldier and provides a new move method. Your assignment should
+     * do the same - but with your own movement strategy.
+     */
+    public class DennisSoldier : Soldier
+    {
+        public int soldier_xPos;
+        public int soldier_yPos;
+
+        private TowerHurtMap unitHurtMap;
+        PathFinding pathFinding = new PathFinding();
+        private List<Node>[] allPaths= new List<Node>[7] ;
+        
+
+        public DennisSoldier(Player player, PlayerLane lane, int x) : base(player, lane, x)
+        {
+
+        }
+
+        /*
+          * This move method is a mere copy of the base movement method.
+          */
+
+        
+        public override void move()
+        {
+            soldier_xPos = posX;
+            soldier_yPos = posY;
+            unitHurtMap = new TowerHurtMap(lane);
+            unitHurtMap.enemyTowerHurtMap = unitHurtMap.GenerateHurtMap();
+
+            //Check if the enemy lane has any towers, if so, alter the movement strategy to be most efficient
+            if (DoesEnemyHaveTowers(unitHurtMap))
+            {
+                moveTo(posX, Math.Clamp(posY + 1, 0, PlayerLane.HEIGHT));
+            }
+            //Otherwise check for a few conditions: //TurnCounter.turnCounter < 250 && 
+            //  1. There were no walls detected
+            //  2. The strategy is considered efficient
+            //  3. Or my backup strategy is not active
+            //Then move according to weighted A* pathfinding
+            else if (!EvaluateScore.backUpStrategy)
+            {
+                if (!unitHurtMap.DetectWalls(unitHurtMap.enemyTowerPositions) || EvaluateScore.efficientOne || EvaluateScore.efficientTwo)
+                {
+
+                    //XmlWriter xmlWriter = XmlWriter.Create("paths.xml");
+
+                    //Because A* is usually used to get the path to 1 goal, I have to generate a path to each of the goals (X:0-6|Y:19) to find the best one
+
+                    for (int i = 0; i < PlayerLane.WIDTH; i++)
+                    {
+                        pathFinding.FindPath(posX, posY, i, 19, unitHurtMap.enemyTowerHurtMap);
+                        allPaths[i] = pathFinding.foundPath;
+
+                    }
+                    //WritePathsToXML(xmlWriter);
+
+                    //After that, calculate which path results in the last damage taken
+                    pathFinding.foundPath = EvaluatePath();
+                    //If a path was found, remove the first node (unit's current position) and move towards the next node
+                    if (pathFinding.foundPath.Count > 0)
+                    {
+                        pathFinding.foundPath.RemoveAt(0);
+                        moveTo(pathFinding.foundPath[0].xPos, pathFinding.foundPath[0].yPos);
+                    }
+                }
+            }              
+            //In case my backup strategy is active
+            else
+            {
+                moveTo(posX, Math.Clamp(posY + 1, 0, PlayerLane.HEIGHT));
+            }
+            
+
+        }
+
+        //Debug method to create an XML file of the paths
+        private void WritePathsToXML(XmlWriter xmlWriter)
+        {
+            xmlWriter.WriteStartDocument();
+
+                xmlWriter.WriteStartElement("Paths");
+                for (int i = 0; i < allPaths.Length; i++)
+                {
+                    xmlWriter.WriteStartElement("Paths" + i);
+                    
+                    foreach (Node n in allPaths[i])
+                    {
+                        xmlWriter.WriteString(n.xPos + "|" + n.yPos + " - ");
+
+                    }
+                    
+                    xmlWriter.WriteEndElement();
+                }
+                xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+        }
+
+        //Evaluate the path with the least amount of damage taken
+        //Because we got 7 potential goals (X=0-6|Y=19), it is important to evaluate each goal
+        private List<Node> EvaluatePath()
+        {
+            
+            List<Node> bestPath = new List<Node>();
+            int[] pathHurtValues = new int[7];
+
+            for(int i = 0; i < allPaths.Length; i++)
+            {
+                int hurtValue = new int();
+                   
+                foreach (Node node in allPaths[i])
+                {
+                    hurtValue += node.hurtValue;
+                }
+                pathHurtValues[i] = hurtValue;
+                
+            }
+            int tempHurt = 100;
+            for (int i = 6; i != 0 ; i--)
+            {
+                
+                if(pathHurtValues[i] < tempHurt)
+                {
+                    tempHurt = pathHurtValues[i];
+                    bestPath = allPaths[i];
+                }
+            }
+            return bestPath;
+        }
+        
+        //Check if the enemy has tower on his defendlane
+        private bool DoesEnemyHaveTowers(TowerHurtMap hurtMap)
+        {
+            int hurt = hurtMap.ReturnWholeHurtMapValue(hurtMap.enemyTowerHurtMap);
+            
+            if(hurt == 0) return true;
+            
+                return false;
+
+
+        }
+
+    }
+}
---
+++ DennisStrategy.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameFramework;
+using System.Linq;
+
+namespace AI_Strategy
+{
+
+    //Basic struct containing information about a tower and its position
+    public struct EnemyTurrets
+    {
+        public Unit tower;
+        public int posX;
+        public int posY;
+
+        public EnemyTurrets(Unit cellTower, int positionX, int positionY) 
+        {
+            this.tower = cellTower;
+            this.posX = positionX;
+            this.posY = positionY;
+        }
+    }
+
+    //Is responsible for creating a hurtmap to indicate how much damage a soldier could take if he would move to a certain field
+    //Crucial to my A* pathfinding
+    public class TowerHurtMap
+    {
+
+        PlayerLane attackLane;
+        private List<EnemyTurrets> enemyTowerStructList = new List<EnemyTurrets>();
+
+        public int[,] enemyTowerPositions = new int[PlayerLane.WIDTH, PlayerLane.HEIGHT];
+        public int[,] enemyTowerHurtMap = new int[PlayerLane.WIDTH, PlayerLane.HEIGHT];
+
+        public TowerHurtMap(PlayerLane laneToGenerate)
+        {
+            this.attackLane = laneToGenerate;
+
+        }
+
+        //Generates a hurtmap of the enemy defendlane
+        //Assign towers as values of 1 to an multidimensional array, which contains the whole enemy field       
+        //Assigning value of +=2 to positions (of multidimensional array), where the range of the tower can hit units -> Generates a hurtmap      
+        
+        public int[,] GenerateHurtMap()
+        {
+            //Because we have no way of knowing if a turret has died, I have to reset the whole enemyTurret list each iteration
+            enemyTowerStructList.Clear();
+
+            //Generate the tower map
+            for (int i = 0; i < PlayerLane.HEIGHT; i++)
+            {
+                for (int k = 0; k < PlayerLane.WIDTH; k++)
+                {
+                    Cell cell = attackLane.GetCellAt(k, i);
+
+                    if (cell.Unit != null && cell.Unit.GetType() == typeof(Tower))
+                    {
+                        enemyTowerPositions[k, i] = 1;
+
+                        //If a tower was detected, create a new EnemyTurrets instance and add it to the list                      
+                        EnemyTurrets tempEnemyTurrets = new EnemyTurrets(cell.Unit, k, i);
+                        if (!enemyTowerStructList.Contains(tempEnemyTurrets))
+                        {
+                            enemyTowerStructList.Add(tempEnemyTurrets);
+                        }
+
+                    }
+                    else
+                    {
+                        enemyTowerPositions[k, i] = 0;
+                    }
+
+                }
+            }
+
+            /*Generate hurt map by increasing the values around a tower in a 5x5 grid by 2 for each tower
+             * Example: T = Tower
+             * 
+             * 2-2-2-2-4-2-2-2-2
+             * 2-2-2-2-4-2-2-2-2
+             * 2-2-T-2-4-2-T-2-2
+             * 2-2-2-2-4-2-2-2-2
+             * 2-2-2-2-4-2-2-2-2
+             */
+            foreach (EnemyTurrets eT in enemyTowerStructList)
+            {
+                for (int x = Math.Clamp(eT.posX - 2, 0, PlayerLane.WIDTH - 1); x <= Math.Clamp(eT.posX + 2, 0, PlayerLane.WIDTH - 1); x++)
+                {
+                    for (int y = Math.Clamp(eT.posY - 2, 0, PlayerLane.HEIGHT - 1); y <= Math.Clamp(eT.posY + 2, 0, PlayerLane.HEIGHT - 1); y++)
+                    {
+                        enemyTowerHurtMap[x, y] += 2;
+                    }
+                }
+            }
+            return enemyTowerHurtMap;
+        }
+        
+        //Debug method to print turrets
+        public void PrintTurrets(int[,] arrayToPrint)
+        {
+            for (int i = 0; i < PlayerLane.HEIGHT; i++)
+            {
+                Console.Write("\n");
+                for (int k = 0; k < PlayerLane.WIDTH; k++)
+                {
+                    if (arrayToPrint[k, i] == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write("|" + arrayToPrint[k, i]);
+                    }
+                    else if (arrayToPrint[k, i] == 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("|" + arrayToPrint[k, i]);
+                    }
+                    else if (arrayToPrint[k, i] > 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("|" + arrayToPrint[k, i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("|" + arrayToPrint[k, i]);
+                    }
+
+
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        //Returns the hurt value of the enemy lane
+        public int ReturnWholeHurtMapValue(int[,] hurtMap)
+        {
+            int wholeHurt = 0;
+            for(int i = 0; i < PlayerLane.WIDTH; i++)
+            {
+                for(int k = 0; k < PlayerLane.HEIGHT; k++)
+                {
+                    wholeHurt += hurtMap[i, k];
+                }
+            }
+            return wholeHurt;
+        }
+
+        //Returns true if a wall was detected, otherwise false
+        public bool DetectWalls(int [,] hurtMap)
+        {           
+            for(int y = 0; y < PlayerLane.HEIGHT; y++)
+            {                           
+                if(hurtMap[0,y] == 1 && hurtMap[1, y] == 1 && hurtMap[2, y] == 1 && hurtMap[3, y] == 1 && hurtMap[4, y] == 1 && hurtMap[5, y] == 1 && hurtMap[6, y] == 1)
+                {
+                    //Console.WriteLine("Walls detected");
+                    
+                    return true;
+                }
+            }
+            //Console.WriteLine("No Walls detected");
+            return false;
+        }
+    }
+
+    //Personal TurnCounter to keep track of current turn. Is necessary due to the protection level of TowerDefense.turns
+    public static class TurnCounter
+    {
+        public static int turnCounter = 0;
+        public static void IncrementTurn() { turnCounter++; }
+    }
+
+    //Evaluates if my score has increased each x turns
+    //Also has some variables that are used by my soldiers (efficientOne/Two, backUpStrategy)
+    //Therefore public static
+    public static class EvaluateScore
+    {
+        public static int previousScore{ get; set; }
+        public static bool efficientOne { get; set; }
+        public static bool efficientTwo { get; set; }
+        public static bool backUpStrategy { get; set; }
+        public static void SetBools()
+        {
+            efficientOne = true;
+            efficientTwo = true;
+            backUpStrategy = false;
+        }
+        
+        public static bool Scoring(Player player)
+        { 
+            if (TurnCounter.turnCounter > 90)
+            {
+
+                //Check if my score increased since the previous score was stored
+                if (player.Score - previousScore >= 3)
+                {
+                    return true;
+
+                }
+                 return false;
+              
+            }
+            return true;
+        }
+}
+
+public class DennisStrategy : AbstractStrategy
+    {
+        private bool defenseBreached = false;
+        bool setUpBools = false;
+
+        TowerHurtMap hurtMap;       
+
+        public DennisStrategy(PlayerLane defendLane, PlayerLane attackLane, Player player) : base(defendLane, attackLane, player)
+        {          
+            this.defendLane = defendLane;
+            this.attackLane = attackLane;
+            this.player = player;
+            
+        }
+        
+        
+        public override void DeployTowers()
+        {
+            //Start my own TurnCounter
+            TurnCounter.IncrementTurn();
+
+            //Only want the efficient bools to be set once
+            if (!setUpBools)
+            {
+                setUpBools = true;
+                EvaluateScore.SetBools();
+            }
+            //Check for breached units = behind my first towerlane
+            List<Unit> breachedUnits = new List<Unit>();
+
+            for(int i = 0; i < PlayerLane.WIDTH; i++)
+            {                              
+                for(int k = 2; k < PlayerLane.HEIGHT; k++)
+                {
+                    Cell cell = defendLane.GetCellAt(i, k);
+                    if (cell.Unit != null && cell.Unit.GetType() == typeof(Soldier))
+                    {                        
+                        breachedUnits.Add(cell.Unit);
+                    }
+                }
+            }
+           
+            // If more than two lanes full of soldiers get through my initial defense
+            // Consider my defense as breached and fall back to second defense line
+            if(breachedUnits.Count >= 14)
+            {
+                defenseBreached = true;
+            }
+
+            //Generate a heatmap for later use
+            hurtMap = new TowerHurtMap(attackLane);
+            hurtMap.enemyTowerHurtMap = hurtMap.GenerateHurtMap();
+
+            //Personally I think this is the best tower placement: Counters f.e. pooling soldiers out of range of my towers and engaging then
+            //Therefore this is rather static
+            if (!defenseBreached)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Tower tower = player.BuyTower(defendLane, i, 2);
+                }
+                for (int i = 4; i < 7; i++)
+                {
+                    Tower newTower = player.BuyTower(defendLane, i, 2);
+                }
+                for (int i = 1; i < 7; i += 2)
+                {
+                    Tower newerTower = player.BuyTower(defendLane, i, 3);
+                }
+            }
+            else
+            {
+                for (int j = PlayerLane.HEIGHT-2; j <= PlayerLane.HEIGHT; j++)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        Tower tower = player.BuyTower(defendLane, i, j);
+                    }
+                    for (int i = 6; i > 2; i--)
+                    {
+                        Tower newTower = player.BuyTower(defendLane, i, j);
+                    }
+                    
+                }
+                
+            }
+
+        }
+
+        public override void DeploySoldiers()
+        {
+            //GetOwnSoldiers();
+            //This is the basis of my strategy evaluation
+            //If the score is rising, the strategy works and Soldiers score
+            //If not, I switch to my backup strategy
+            //Evaluation starts after 100 turns because I have to create towers myself
+
+            //Every 14 turns, check for progress in the score department           
+            if (TurnCounter.turnCounter % 14 == 0)
+            {
+                if (EvaluateScore.efficientOne)
+                {
+                    EvaluateScore.efficientOne = EvaluateScore.Scoring(player);
+                }
+                else
+                {
+                    EvaluateScore.efficientTwo = EvaluateScore.Scoring(player);
+                }
+            }
+
+            //Every 10 turns set the current score as previous score 
+            if (TurnCounter.turnCounter % 10  == 0)
+            {                
+                //Console.WriteLine(EvaluateScore.previousScore);
+                EvaluateScore.previousScore = player.Score;
+            }
+
+            //---Strategy A--- Strategy versus non-wall using enemies
+            //If there are no towers on the map, or my soldier score and no walls detected 
+            //Spawn waves of soldiers every 14 gold
+            hurtMap.GenerateHurtMap();
+            if (!hurtMap.DetectWalls(hurtMap.enemyTowerPositions) && !EvaluateScore.backUpStrategy)
+            {
+                if (hurtMap.ReturnWholeHurtMapValue(hurtMap.enemyTowerHurtMap) == 0 || EvaluateScore.efficientOne || EvaluateScore.efficientTwo )
+                {
+                    if (player.Gold > 14)
+                    {
+                        for (int i = 0; i < PlayerLane.WIDTH; i++)
+                        {
+                            Soldier soldier = this.player.BuySoldier(attackLane, i);
+
+                        }
+                    }
+                }
+            }           
+            //---Strategy B--- Strategy against enemies that use a (double) wall to pool money and spam soldiers at turn >900
+            //If my first strategy is not succesful and my score does not increase
+            //Pool money until turn 750, then use that money to diminish all the saved money of the enemy, destroy his base and win the game
+            else
+            {
+                EvaluateScore.backUpStrategy = true;
+                if (TurnCounter.turnCounter > 750)
+                {                       
+                    for (int i = 0; i < PlayerLane.WIDTH; i++)
+                    {
+                        Soldier soldier = this.player.BuySoldier(attackLane, i);
+                        
+                    } 
+                }
+            }
+            
+        }
+
+        public override List<Soldier> SortedSoldierArray(List<Soldier> unsortedList)
+        {        
+            //foreach(DennisSoldier ds in unsortedList)
+            //{
+            //    unsortedList.OrderBy(DennisSoldier => ds.soldier_yPos).ThenBy(DennisSoldier => ds.soldier_yPos);
+            //}
+
+            return unsortedList;
+        }    
+    }
+}
